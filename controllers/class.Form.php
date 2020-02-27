43f1f3daece2294ed2591bbc2091054014a15879
Xopher <cdpollard@gmail.com>
2020-02-27 14:36:22 -0700

    :hammer: refactor Form class.

--- controllers/class.Form.php
+++ controllers/class.Form.php
@@ -2,28 +2,17 @@
   class Form{
     private $ext = 'html';
 
-    function __construct(){
-      $this->init();
-    }
-
-    function init(){
-    }
-
     function loadForm($form){
-      $ext = $this->ext;
       switch($form){
         case 'login':
-          $form = "login.{$ext}";
-        break;
         case 'new-user':
-          $form = "{$form}.{$ext}";
-        break;
         case 'profile':
-          $form = "{$form}.{$ext}";
-        break;
+          $form = "views/{$form}.{$this->ext}";
+          break;
         default:
           die('Invalid Form');
       }
-      return file_get_contents("views/".$form);
+
+      return file_get_contents($form);
     }
   }
